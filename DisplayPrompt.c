0a6b1e305829901a3da465fc7e38f4218f66a827
mwihoti <danielmwihoti@gmail.com>
2023-12-10 21:14:59 -0800

    getinfo

--- DisplayPrompt.c
+++ DisplayPrompt.c
@@ -5,12 +5,16 @@ int main(int ac, char **argv)
 
 	char *prompt = "$ ";
 	char *lineptr = NULL ;
+	char *lineptr_copy = NULL;
 	size_t n = 0;
 	ssize_t nchars_read;
+	const char *delim = "\n";
+	int num_tokens = 0;
+	char *token;
+	int i;
 
 	(void)ac;
 
-	(void)argv;
 	while (1)
 	{
 	printf("%s", prompt);
@@ -20,9 +24,38 @@ int main(int ac, char **argv)
 		printf("Exited shell\n");
 		return (-1);
 	}
-	printf("%s\n", lineptr);
-	free(lineptr);
-	lineptr = NULL;
+	lineptr_copy = malloc(sizeof(char) * nchars_read);
+	if (lineptr_copy == NULL)
+	{
+		perror("tsh: memory error");
+		return (-1);
 	}
+	strcpy(lineptr_copy, lineptr);
+
+	token = strtok(lineptr, delim);
+
+	while (token != NULL)
+	{
+		num_tokens++;
+		token = strtok(NULL, delim);
+	}
+	num_tokens++;
+
+	argv = malloc(sizeof(char *) * num_tokens);
+
+	token = strtok(lineptr_copy, delim);
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+
+		execmd(argv);
+
+	}	
+	free(lineptr);
+	free(lineptr_copy);
 	return (0);
 }
--- main.h
+++ main.h
@@ -1,5 +1,116 @@
+#ifndef _MAIN_H_
+#define  _MAIN_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include<fcnt1.h>
+#include <sys/stat>
+#include <sys/types.h>
+
+/* read/write buffer */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/*commands */
+#define CMD_NORM 0
+#define CMD_OR 1
+#define CMD_AND 2
+#define CMD_CHAIN 3
+
+/* convert_number */
+
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
+#define  USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE "Shell history by Mwihoti"
+#define HIST_MAX 4096
+
+
+extern char **environ;
+
+/**
+ * struct linkedlist - singly linked list
+ * @num: number field
+ * @str: string
+ * @next: points to the next node
+ */
+typedef struct linkedlist 
+{
+	int num;
+	char *str;
+	struct linkedlist *next;
+} list_t;
+/**
+ * struct passinfoarg - pseudo-arguements to pass into a function
+ * @arg: generated from getline string
+ * @argv: an array of strings generated from arg
+ * @path: string path for the current command
+ * @argc: arguement count
+ * @line_count: error count
+ * @err_num: error code for exit
+ * @linecount_flag: counts line of input
+ * @fname: program filename
+ * @env: copy of environ linked list
+ * @environ: custom modified copy of environ
+ * @history: history mode
+ * @alias: alias node
+ * @env_changed: check if environ changed
+ * @status: return status of last exec command
+ * @cmd_buf: address of pointer to cmd_buf
+ * @cmd_buf_type: CMD_type || &&
+ * @readfd: fd from which to read line input
+ * @histcount: history line number count
+ */
+typedef struct passinfoarg
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+	char **cmd_buf;
+	int cmd_buf_type;
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+/**
+ * struct builtin - contains a builtin string
+ * @type: builtin command flag
+ * @func: function
+ */
+typedef struct builtin
+{
+	char  *type;
+	int (*func)(info_t *);
+} builtin_table;
+
+/*
+
+void execmd(char **argv);
+
+#endif
+
 
 
